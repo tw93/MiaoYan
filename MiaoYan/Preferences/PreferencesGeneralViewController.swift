7a9a1a0c16cf6bec78d1848919a12dbc8b64bb8e
Tw93 <tw93@qq.com>
2020-04-11 18:43:29 +0800

    update

--- MiaoYan/Preferences/PreferencesGeneralViewController.swift
+++ MiaoYan/Preferences/PreferencesGeneralViewController.swift
@@ -16,10 +16,7 @@ class PreferencesGeneralViewController: NSViewController {
         super.viewWillAppear()
         preferredContentSize = NSSize(width: 476, height: 413)
     }
-    @IBOutlet var newNoteshortcutView: MASShortcutView!
-    @IBOutlet var searchNotesShortcut: MASShortcutView!
     @IBOutlet weak var defaultStoragePath: NSPathControl!
-    @IBOutlet weak var fileFormat: NSPopUpButton!
 
     //MARK: global variables
 
@@ -27,7 +24,6 @@ class PreferencesGeneralViewController: NSViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        initShortcuts()
     }
 
     override func viewDidAppear() {
@@ -76,87 +72,5 @@ class PreferencesGeneralViewController: NSViewController {
         task.arguments = [path]
         task.launch()
         exit(0)
-    }
-
-    func initShortcuts() {
-        guard let vc = ViewController.shared() else { return }
-
-        let mas = MASShortcutMonitor.shared()
-
-        newNoteshortcutView.shortcutValue = UserDefaultsManagement.newNoteShortcut
-        searchNotesShortcut.shortcutValue = UserDefaultsManagement.searchNoteShortcut
-
-        newNoteshortcutView.shortcutValueChange = { (sender) in
-            if ((self.newNoteshortcutView.shortcutValue) != nil) {
-                mas?.unregisterShortcut(UserDefaultsManagement.newNoteShortcut)
-
-                let keyCode = self.newNoteshortcutView.shortcutValue.keyCode
-                let modifierFlags = self.newNoteshortcutView.shortcutValue.modifierFlags
-
-                UserDefaultsManagement.newNoteShortcut = MASShortcut(keyCode: keyCode, modifierFlags: modifierFlags)
-
-                MASShortcutMonitor.shared().register(self.newNoteshortcutView.shortcutValue, withAction: {
-                    vc.makeNoteShortcut()
-                })
-            }
-        }
-
-        searchNotesShortcut.shortcutValueChange = { (sender) in
-            if ((self.searchNotesShortcut.shortcutValue) != nil) {
-                mas?.unregisterShortcut(UserDefaultsManagement.searchNoteShortcut)
-
-                let keyCode = self.searchNotesShortcut.shortcutValue.keyCode
-                let modifierFlags = self.searchNotesShortcut.shortcutValue.modifierFlags
-
-                UserDefaultsManagement.searchNoteShortcut = MASShortcut(keyCode: keyCode, modifierFlags: modifierFlags)
-
-                MASShortcutMonitor.shared().register(self.searchNotesShortcut.shortcutValue, withAction: {
-                    vc.searchShortcut()
-                })
-            }
-        }
-    }
-
-    @IBAction func showDockIcon(_ sender: NSButton) {
-        let isEnabled = sender.state == .on
-        UserDefaultsManagement.showDockIcon = isEnabled
-
-        NSApp.setActivationPolicy(isEnabled ? .regular : .accessory)
-
-        DispatchQueue.main.async {
-            NSMenu.setMenuBarVisible(true)
-            NSApp.activate(ignoringOtherApps: true)
-        }
-    }
-
-    @IBAction func txtAsMarkdown(_ sender: NSButton) {
-        UserDefaultsManagement.txtAsMarkdown = sender.state == .on
-    }
-
-    @IBAction func showInMenuBar(_ sender: NSButton) {
-        UserDefaultsManagement.showInMenuBar = sender.state == .on
-
-        guard let appDelegate = NSApplication.shared.delegate as? AppDelegate else { return }
-
-        if sender.state == .off {
-            appDelegate.removeMenuBar(nil)
-            return
-        }
-
-        appDelegate.addMenuBar(nil)
-    }
-
-    @IBAction func fileFormat(_ sender: NSPopUpButton) {
-        guard let item = sender.selectedItem else { return }
-
-        UserDefaultsManagement.fileFormat = NoteType.withTag(rawValue: item.tag)
-    }
-
-    @IBAction func fileContainer(_ sender: NSPopUpButton) {
-        guard let item = sender.selectedItem else { return }
-
-        if let container = NoteContainer(rawValue: item.tag) {
-            UserDefaultsManagement.fileContainer = container
-        }
     }
 }
