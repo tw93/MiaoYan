9700cd20698b36a86b46d24c83669ca713828bb8
Tw93 <tw93@qq.com>
2020-05-24 23:12:44 +0800

    改成根目录

--- Mac/SidebarScrollView.swift
+++ Mac/SidebarScrollView.swift
@@ -1,11 +1,3 @@
-//
-//  SidebarScrollView.swift
-//  FSNotes
-//
-//  Created by Oleksandr Glushchenko on 4/9/18.
-//  Copyright © 2018 Oleksandr Glushchenko. All rights reserved.
-//
-
 import Cocoa
 
 class SidebarNotesView: NSView {
--- Mac/View/SidebarHeaderCellView.swift
+++ Mac/View/SidebarHeaderCellView.swift
@@ -1,11 +1,3 @@
-//
-//  SidebarHeaderCellView.swift
-//  FSNotes
-//
-//  Created by Олександр Глущенко on 15.10.2019.
-//  Copyright © 2019 Oleksandr Glushchenko. All rights reserved.
-//
-
 import Cocoa
 
 class SidebarHeaderCellView: NSTableCellView {
