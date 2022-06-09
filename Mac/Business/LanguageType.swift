192bf756d8ed9f3cd2f12be58ed539dbbcef4b97
Tw93 <tw93@qq.com>
2022-06-09 11:52:49 +0800

    :sparkles: 优化一下字体

--- Mac/Business/LanguageType.swift
+++ Mac/Business/LanguageType.swift
@@ -7,23 +7,23 @@ enum LanguageType: Int {
     var description: String {
         switch self.rawValue {
         case 0x00: return "English"
-        case 0x06: return "Chinese"
-        default: return "Chinese"
+        case 0x06: return "Chinese (Simplified)"
+        default: return ""
         }
     }
     
     var code: String {
         switch self.rawValue {
-        case 0x00: return "English"
-        case 0x06: return "Chinese"
-        default: return "Chinese"
+        case 0x00: return "en"
+        case 0x06: return "zh-Hans"
+        default: return "zh-Hans"
         }
     }
     
     static func withName(rawValue: String) -> LanguageType {
         switch rawValue {
         case "English": return LanguageType.English
-        case "Chinese": return LanguageType.Chinese
+        case "Chinese (Simplified)": return LanguageType.Chinese
         default: return LanguageType.Chinese
         }
     }
