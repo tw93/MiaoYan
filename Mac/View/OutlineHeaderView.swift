796983842a1e8849b5dee70df8a494c978827fc0
Tw93 <tw93@qq.com>
2022-06-13 22:27:42 +0800

    :sparkles: 视觉优化,字体优化

--- Mac/View/OutlineHeaderView.swift
+++ Mac/View/OutlineHeaderView.swift
@@ -3,9 +3,9 @@ import Cocoa
 class OutlineHeaderView: NSView {
     override func draw(_ dirtyRect: NSRect) {
        super.draw(dirtyRect)
-        
+
        let lightColor = NSColor(red:1.00, green:1.00, blue:1.00, alpha:1.0)
-       let darkColor = NSColor(red:0.13, green:0.15, blue:0.17, alpha:1.0)
+       let darkColor = NSColor(red:0.09, green:0.11, blue:0.13, alpha:1.0)
 
        if NSAppearance.current.isDark {
           darkColor.setFill()
