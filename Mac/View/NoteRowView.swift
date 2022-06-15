095dcb48d47c9f593ad92cd984af033b36a3e6b3
Tw93 <tw93@qq.com>
2022-06-15 11:19:08 +0800

    :bug: 兼容性显示错误

--- Mac/View/NoteRowView.swift
+++ Mac/View/NoteRowView.swift
@@ -1,24 +1,27 @@
 import Cocoa
 
 class NoteRowView: NSTableRowView {
-
     override func draw(_ dirtyRect: NSRect) {
         super.draw(dirtyRect)
     }
     
     override func drawSelection(in dirtyRect: NSRect) {
         if self.selectionHighlightStyle != .none {
-            let selectionRect = NSInsetRect(self.bounds, 10, 0.5)
+            var selectionRect = NSInsetRect(self.bounds, 10, 0.5)
             
             if NSAppearance.current.isDark {
                 NSColor(red: 0.27, green: 0.27, blue: 0.27, alpha: 1).setStroke()
                 NSColor(red: 0.27, green: 0.27, blue: 0.27, alpha: 1).setFill()
             } else {
-                NSColor(red: 0.86, green:0.86, blue: 0.86, alpha: 1).setStroke()
-                NSColor(red: 0.86, green:0.86, blue: 0.86, alpha: 1).setFill()
+                NSColor(red: 0.86, green: 0.86, blue: 0.86, alpha: 1).setStroke()
+                NSColor(red: 0.86, green: 0.86, blue: 0.86, alpha: 1).setFill()
             }
-            
-            let selectionPath = NSBezierPath.init(roundedRect: selectionRect, xRadius: 4, yRadius: 4)
+            var selectionPath = NSBezierPath(roundedRect: selectionRect, xRadius: 4, yRadius: 4)
+            if #available(OSX 10.15.8, *) {} else {
+                selectionRect = NSInsetRect(self.bounds, 0, 0)
+                selectionPath = NSBezierPath(roundedRect: selectionRect, xRadius: 0, yRadius: 0)
+            }
+           
             selectionPath.fill()
             selectionPath.stroke()
         }
