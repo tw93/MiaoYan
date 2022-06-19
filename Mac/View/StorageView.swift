fa6be71e9e6eb24911ad31b86ea6ebc869a8fad8
Tw93 <tw93@qq.com>
2022-06-19 18:11:36 +0800

    :art: 更新文档

--- Mac/View/StorageView.swift
+++ Mac/View/StorageView.swift
@@ -7,8 +7,8 @@ class StorageView: NSView {
         layer?.backgroundColor = NSColor(red:0.96, green:0.96, blue:0.96, alpha:1.0).cgColor
     }
     override func awakeFromNib() {
-        var f = self.frame; 
-        f.size.width = 128;
+        var f = self.frame;
+        f.size.width = 138;
         self.frame = f;
         self.setFrameSize(f.size)
         self.setBoundsSize(f.size)
