3738dc54e41c2b6cbb23788ca40a4332cd9f64df
Tw93 <tw93@qq.com>
2022-06-19 17:38:05 +0800

    :sparkles: 初始化的时候第一次设置合适的宽度

--- Mac/View/StorageView.swift
+++ Mac/View/StorageView.swift
@@ -6,4 +6,11 @@ class StorageView: NSView {
         super.draw(dirtyRect)
         layer?.backgroundColor = NSColor(red:0.96, green:0.96, blue:0.96, alpha:1.0).cgColor
     }
+    override func awakeFromNib() {
+        var f = self.frame; 
+        f.size.width = 128;
+        self.frame = f;
+        self.setFrameSize(f.size)
+        self.setBoundsSize(f.size)
+    }
 }
