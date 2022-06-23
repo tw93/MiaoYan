0f286a3438918ff4646516f6ec306ce79e0fadcf
Tw93 <tw93@qq.com>
2022-06-23 19:53:45 +0800

    :bug: 终于修复分割虚线的小bug

--- Mac/View/EditorSplitView.swift
+++ Mac/View/EditorSplitView.swift
@@ -12,20 +12,6 @@ class EditorSplitView: NSSplitView, NSSplitViewDelegate {
         return 0
     }
 
-    override var dividerColor: NSColor {
-        if UserDefaultsManagement.appearanceType != AppearanceType.Custom, #available(OSX 10.13, *) {
-            return NSColor.init(named: "divider")!
-        } else {
-            return NSColor(red:0.83, green:0.83, blue:0.83, alpha:1.0)
-        }
-    }
-
-    override var dividerThickness: CGFloat {
-        get {
-            return shouldHideDivider ? 0 : 1
-        }
-    }
-
     func splitViewDidResizeSubviews(_ notification: Notification) {
         ViewController.shared()?.viewDidResize()
     }
