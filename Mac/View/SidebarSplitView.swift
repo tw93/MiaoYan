ee06649bd0a9cac43b29488f587eef829de8b7b8
Tw93 <tw93@qq.com>
2022-06-23 19:53:45 +0800

    :bug: 终于修复分割虚线的小bug

--- Mac/View/SidebarSplitView.swift
+++ Mac/View/SidebarSplitView.swift
@@ -1,13 +1,3 @@
 import Cocoa
 
-class SidebarSplitView: NSSplitView {
-    
-    override var dividerColor: NSColor {
-        if UserDefaultsManagement.appearanceType != AppearanceType.Custom, #available(OSX 10.13, *) {
-            return NSColor.init(named: "divider")!
-        } else {
-            return NSColor(red:0.83, green:0.83, blue:0.83, alpha:1.0)
-        }
-    }
-}
-
+class SidebarSplitView: NSSplitView {}
