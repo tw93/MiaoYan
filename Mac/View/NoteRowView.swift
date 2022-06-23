8083c1f88a0c76526cf1869ff329e17438ff1741
Tw93 <tw93@qq.com>
2022-06-23 15:47:06 +0800

    :sparkles: 优化列表的展示

--- Mac/View/NoteRowView.swift
+++ Mac/View/NoteRowView.swift
@@ -5,25 +5,10 @@ class NoteRowView: NSTableRowView {
         super.draw(dirtyRect)
     }
     
-    override func drawSelection(in dirtyRect: NSRect) {
-        if self.selectionHighlightStyle != .none {
-            var selectionRect = NSInsetRect(self.bounds, 10, 0.5)
-            
-            if NSAppearance.current.isDark {
-                NSColor(red: 0.27, green: 0.27, blue: 0.27, alpha: 1).setStroke()
-                NSColor(red: 0.27, green: 0.27, blue: 0.27, alpha: 1).setFill()
-            } else {
-                NSColor(red: 0.86, green: 0.86, blue: 0.86, alpha: 1).setStroke()
-                NSColor(red: 0.86, green: 0.86, blue: 0.86, alpha: 1).setFill()
-            }
-            var selectionPath = NSBezierPath(roundedRect: selectionRect, xRadius: 4, yRadius: 4)
-            if #available(OSX 10.15.8, *) {} else {
-                selectionRect = NSInsetRect(self.bounds, 0, 0)
-                selectionPath = NSBezierPath(roundedRect: selectionRect, xRadius: 0, yRadius: 0)
-            }
-           
-            selectionPath.fill()
-            selectionPath.stroke()
+    override var isEmphasized: Bool {
+        set {}
+        get {
+            false
         }
     }
 }
