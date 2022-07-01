3e957cdbaa5cc2a1c5d2d7ea3511471484860ac0
Tw93 <tw93@qq.com>
2022-07-01 15:17:21 +0800

    :bug: 黑色主题下中英文链接颜色

--- Mac/MPreviewView.swift
+++ Mac/MPreviewView.swift
@@ -233,7 +233,7 @@ class MPreviewView: WKWebView, WKUIDelegate, WKNavigationDelegate {
 
         let pageHTMLString = try htmlFromTemplate(htmlString, css: css)
 
-        // print(pageHTMLString)
+        //  print(pageHTMLString)
         let indexURL = createTemporaryBundle(pageHTMLString: pageHTMLString)
 
         if let i = indexURL {
