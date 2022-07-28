d7e6ecfbd15afd191b81ef018476c85635a29f63
Tw93 <tw93@qq.com>
2022-07-28 22:20:11 +0800

    :bug: 修复当前文档预览切换时候标题光标可能会闪

--- Mac/ViewController.swift
+++ Mac/ViewController.swift
@@ -7,14 +7,15 @@ import MiaoYanCore_macOS
 import WebKit
 
 class ViewController: NSViewController,
-        NSTextViewDelegate,
-        NSPopoverDelegate,
-        NSTextFieldDelegate,
-        NSSplitViewDelegate,
-        NSOutlineViewDelegate,
-        NSOutlineViewDataSource,
-        WebFrameLoadDelegate,
-        NSMenuItemValidation, NSUserNotificationCenterDelegate {
+    NSTextViewDelegate,
+    NSPopoverDelegate,
+    NSTextFieldDelegate,
+    NSSplitViewDelegate,
+    NSOutlineViewDelegate,
+    NSOutlineViewDataSource,
+    WebFrameLoadDelegate,
+    NSMenuItemValidation, NSUserNotificationCenterDelegate
+{
     // MARK: - Properties
 
     public var fsManager: FileSystemEventManager?
@@ -38,8 +39,7 @@ class ViewController: NSViewController,
     private var updateViews = [Note]()
 
     override var representedObject: Any? {
-        didSet {
-        }
+        didSet {}
         // Update the view, if already loaded.
     }
 
@@ -73,7 +73,7 @@ class ViewController: NSViewController,
             layer.backgroundColor = .clear
             titleBarAdditionalView.wantsLayer = true
             titleBarAdditionalView.layer = layer
-            if (UserDefaultsManagement.buttonShow == "Hover") {
+            if UserDefaultsManagement.buttonShow == "Hover" {
                 titleBarAdditionalView.alphaValue = 0
             } else {
                 titleBarAdditionalView.alphaValue = 1
@@ -88,7 +88,7 @@ class ViewController: NSViewController,
             layer.backgroundColor = .clear
             addProjectButton.wantsLayer = true
             addProjectButton.layer = layer
-            if (UserDefaultsManagement.buttonShow == "Hover") {
+            if UserDefaultsManagement.buttonShow == "Hover" {
                 addProjectButton.alphaValue = 0
             } else {
                 addProjectButton.alphaValue = 1
@@ -131,7 +131,7 @@ class ViewController: NSViewController,
     @IBOutlet var titleBarView: TitleBarView! {
         didSet {
             titleBarView.onMouseExitedClosure = { [weak self] in
-                if (UserDefaultsManagement.buttonShow != "Hover") {
+                if UserDefaultsManagement.buttonShow != "Hover" {
                     return
                 }
                 DispatchQueue.main.async {
@@ -142,7 +142,7 @@ class ViewController: NSViewController,
                 }
             }
             titleBarView.onMouseEnteredClosure = { [weak self] in
-                if (UserDefaultsManagement.buttonShow != "Hover") {
+                if UserDefaultsManagement.buttonShow != "Hover" {
                     return
                 }
                 DispatchQueue.main.async {
@@ -158,7 +158,7 @@ class ViewController: NSViewController,
     @IBOutlet var projectHeaderView: OutlineHeaderView! {
         didSet {
             projectHeaderView.onMouseExitedClosure = { [weak self] in
-                if (UserDefaultsManagement.buttonShow != "Hover") {
+                if UserDefaultsManagement.buttonShow != "Hover" {
                     return
                 }
                 DispatchQueue.main.async {
@@ -169,7 +169,7 @@ class ViewController: NSViewController,
                 }
             }
             projectHeaderView.onMouseEnteredClosure = { [weak self] in
-                if (UserDefaultsManagement.buttonShow != "Hover") {
+                if UserDefaultsManagement.buttonShow != "Hover" {
                     return
                 }
                 DispatchQueue.main.async {
@@ -211,14 +211,13 @@ class ViewController: NSViewController,
         let popoverWindowY = popover.contentViewController?.view.window?.frame.origin.y ?? 0
 
         popover.contentViewController?.view.window?.setFrameOrigin(
-                NSPoint(x: popoverWindowX + 18, y: popoverWindowY)
+            NSPoint(x: popoverWindowX + 18, y: popoverWindowY)
         )
 
         popover.contentViewController?.view.window?.makeKey()
     }
 
-    @objc func detachedWindowWillClose(notification: NSNotification) {
-    }
+    @objc func detachedWindowWillClose(notification: NSNotification) {}
 
     private var popoverVisible: Bool {
         popover.isShown
@@ -857,10 +856,11 @@ class ViewController: NSViewController,
 
         // Focus search bar on ESC
         if
-                event.characters == ".",
-                event.modifierFlags.contains(.command),
+            event.characters == ".",
+            event.modifierFlags.contains(.command),
 
-                NSApplication.shared.mainWindow == NSApplication.shared.keyWindow {
+            NSApplication.shared.mainWindow == NSApplication.shared.keyWindow
+        {
             UserDataService.instance.resetLastSidebar()
 
             if let view = NSApplication.shared.mainWindow?.firstResponder as? NSTextView, let textField = view.superview?.superview, textField.isKind(of: NameTextField.self) {
@@ -932,7 +932,8 @@ class ViewController: NSViewController,
         }
 
         if let fr = mw.firstResponder, !fr.isKind(of: EditTextView.self), !fr.isKind(of: NSTextView.self), !event.modifierFlags.contains(.command),
-           !event.modifierFlags.contains(.control) {
+           !event.modifierFlags.contains(.control)
+        {
             if let char = event.characters {
                 let newSet = CharacterSet(charactersIn: char)
                 if newSet.isSubset(of: CharacterSet.alphanumerics) {
@@ -1189,7 +1190,8 @@ class ViewController: NSViewController,
         vc.storage.removeNotes(notes: notes) { urls in
 
             if let appd = NSApplication.shared.delegate as? AppDelegate,
-               let md = appd.mainWindowController {
+               let md = appd.mainWindowController
+            {
                 let undoManager = md.notesListUndoManager
 
                 if let ntv = vc.notesTableView {
@@ -1510,7 +1512,7 @@ class ViewController: NSViewController,
         }
 
         if let sidebarItem = vc.getSidebarItem(), sidebarItem.isTrash() {
-            let indexSet = IndexSet(integersIn: 0..<vc.notesTableView.noteList.count)
+            let indexSet = IndexSet(integersIn: 0 ..< vc.notesTableView.noteList.count)
             vc.notesTableView.removeRows(at: indexSet, withAnimation: .effectFade)
         }
 
@@ -1528,7 +1530,8 @@ class ViewController: NSViewController,
         }
 
         if let controller = vc.storyboard?.instantiateController(withIdentifier: "ProjectSettingsViewController")
-                as? ProjectSettingsViewController {
+            as? ProjectSettingsViewController
+        {
             projectSettingsViewController = controller
 
             if let project = vc.getSidebarProject() {
@@ -1682,8 +1685,7 @@ class ViewController: NSViewController,
 
     private var selectRowTimer = Timer()
 
-    func updateTable(search: Bool = false, searchText: String? = nil, sidebarItem: SidebarItem? = nil, projects: [Project]? = nil, completion: @escaping () -> Void = {
-    }) {
+    func updateTable(search: Bool = false, searchText: String? = nil, sidebarItem: SidebarItem? = nil, projects: [Project]? = nil, completion: @escaping () -> Void = {}) {
         var sidebarItem: SidebarItem? = sidebarItem
         var projects: [Project]? = projects
         var sidebarName: String?
@@ -1708,8 +1710,9 @@ class ViewController: NSViewController,
         // Global search if sidebar not checked
         if type == nil,
            projects == nil || (
-                   projects!.count < 2 && projects!.first!.isRoot
-           ) {
+               projects!.count < 2 && projects!.first!.isRoot
+           )
+        {
             type = .All
         }
 
@@ -1825,18 +1828,18 @@ class ViewController: NSViewController,
         }
 
         return !note.name.isEmpty
-                && (filter.isEmpty || isMatched(note: note, terms: terms!)
-        ) && (
+            && (filter.isEmpty || isMatched(note: note, terms: terms!)
+            ) && (
                 type == .All && note.project.showInCommon
-                        || (
+                    || (
                         type != .All && projects!.contains(note.project)
-                                || (note.project.parent != nil && projects!.contains(note.project.parent!))
-                )
-                        || type == .Trash
-        ) && (
+                            || (note.project.parent != nil && projects!.contains(note.project.parent!))
+                    )
+                    || type == .Trash
+            ) && (
                 type == .Trash && note.isTrash()
-                        || type != .Trash && !note.isTrash()
-        )
+                    || type != .Trash && !note.isTrash()
+            )
     }
 
     public func contains(tag name: String, in tags: [String]) -> Bool {
@@ -1920,9 +1923,10 @@ class ViewController: NSViewController,
         }
 
         if
-                NSApplication.shared.isActive,
-                !NSApplication.shared.isHidden,
-                !mainWindow.isMiniaturized {
+            NSApplication.shared.isActive,
+            !NSApplication.shared.isHidden,
+            !mainWindow.isMiniaturized
+        {
             NSApplication.shared.hide(nil)
             return
         }
@@ -2062,15 +2066,15 @@ class ViewController: NSViewController,
         }
 
         let nowUnpinned = updatedNotes
-                .filter { _, note -> Bool in
-                    !note.isPinned
+            .filter { _, note -> Bool in
+                !note.isPinned
+            }
+            .compactMap { _, note -> (Int, Note)? in
+                guard let curRow = state.firstIndex(where: { $0 === note }) else {
+                    return nil
                 }
-                .compactMap { _, note -> (Int, Note)? in
-                    guard let curRow = state.firstIndex(where: { $0 === note }) else {
-                        return nil
-                    }
-                    return (curRow, note)
-                }
+                return (curRow, note)
+            }
         for (row, note) in nowUnpinned.reversed() {
             guard let newRow = resorted.firstIndex(where: { $0 === note }) else {
                 continue
@@ -2173,10 +2177,12 @@ class ViewController: NSViewController,
         }
         editor.subviews.removeAll(where: { $0.isKind(of: MPreviewView.self) })
         refillEditArea()
+        DispatchQueue.main.async {
+            self.titleLabel.isEditable = true
+        }
         if !isFocusedTitle {
             focusEditArea()
         }
-        titleLabel.isEditable = true
     }
 
     func togglePreview() {
@@ -2296,7 +2302,8 @@ class ViewController: NSViewController,
 
         for menu in noteMenu.items {
             if let identifier = menu.identifier?.rawValue,
-               personalSelection.contains(identifier) {
+               personalSelection.contains(identifier)
+            {
                 menu.isHidden = (vc.notesTableView.selectedRowIndexes.count > 1)
             }
         }
@@ -2309,11 +2316,11 @@ class ViewController: NSViewController,
         let sortByLabel = NSLocalizedString("Sort by", comment: "View menu")
 
         guard
-                let menu = NSApp.menu,
-                let view = menu.item(withTitle: viewLabel),
-                let submenu = view.submenu,
-                let sortMenu = submenu.item(withTitle: sortByLabel),
-                let sortItems = sortMenu.submenu
+            let menu = NSApp.menu,
+            let view = menu.item(withTitle: viewLabel),
+            let submenu = view.submenu,
+            let sortMenu = submenu.item(withTitle: sortByLabel),
+            let sortItems = sortMenu.submenu
         else {
             return
         }
