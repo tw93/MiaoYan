5de984da9d56ad4fe8df61b2c13919cbc0340e2b
Tw93 <tw93@qq.com>
2022-08-15 21:39:42 +0800

    :sparkles: 新版本的兼容适配终于好了

--- Mac/View/NoteRowView.swift
+++ Mac/View/NoteRowView.swift
@@ -1,10 +1,6 @@
 import Cocoa
 
 class NoteRowView: NSTableRowView {
-    override func draw(_ dirtyRect: NSRect) {
-        super.draw(dirtyRect)
-    }
-    
     override var isEmphasized: Bool {
         set {}
         get {
