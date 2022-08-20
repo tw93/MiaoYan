1235cd7d05b89a0ced23f0a3af028c1d919b7c50
Tw93 <tw93@qq.com>
2022-08-20 08:43:37 +0800

    光标兼容

--- Mac/ViewController.swift
+++ Mac/ViewController.swift
@@ -1874,17 +1874,26 @@ class ViewController:
         notesTableView.scrollRowToVisible(0)
     }
 
+    
     func focusEditArea(firstResponder: NSResponder? = nil) {
         guard EditTextView.note != nil else { return }
-
-        editArea.window?.makeFirstResponder(editArea)
+        var resp: NSResponder = editArea
+        if let responder = firstResponder {
+            resp = responder
+        }
 
         if notesTableView.selectedRow > -1 {
             DispatchQueue.main.async {
                 self.editArea.isEditable = true
                 self.emptyEditAreaView.isHidden = true
+                self.titleBarView.isHidden = false
+                self.editArea.window?.makeFirstResponder(resp)
+                self.editArea.restoreCursorPosition()
             }
+            return
         }
+
+        editArea.window?.makeFirstResponder(resp)
     }
 
     func focusSearchInput(firstResponder: NSResponder? = nil) {
