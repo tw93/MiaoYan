84f0accc0084f5029d45f55c4f2aab9af96e48e2
Tw93 <tw93@qq.com>
2022-09-29 11:04:01 +0800

    :bug: 切换首次打开字体变化

--- Mac/Helpers/UserDefaultsManagement.swift
+++ Mac/Helpers/UserDefaultsManagement.swift
@@ -34,7 +34,7 @@ public enum UserDefaultsManagement {
     static var editorLineSpacing = 3.0
     static var editorLineHeight = 1.3
     static var editorLetterSpacing = 0.56
-    static var windowLetterSpacing = 0.38
+    static var windowLetterSpacing = 0.4
 
     static var titleFontSize = 20
     static var emptyEditTitleFontSize = 36
