59cff8a3f34f375afba8ef90aa4a579311489fb6
Tw93 <tw93@qq.com>
2023-03-13 19:30:20 +0800

    :bug: 修复10.15兼容问题

--- Common/Shared/Extensions/String+.swift
+++ Common/Shared/Extensions/String+.swift
@@ -1,4 +1,5 @@
 import CommonCrypto
+import CryptoKit
 import Foundation
 
 public extension String {
@@ -108,13 +109,9 @@ public extension String {
     }
 
     var md5: String {
-        let data = Data(utf8)
-        let hash = data.withUnsafeBytes { (bytes: UnsafeRawBufferPointer) -> [UInt8] in
-            var hash = [UInt8](repeating: 0, count: Int(CC_MD5_DIGEST_LENGTH))
-            CC_MD5(bytes.baseAddress, CC_LONG(data.count), &hash)
-            return hash
-        }
-        return hash.map { String(format: "%02x", $0) }.joined()
+        let data = Data(self.utf8)
+        let hash = Insecure.MD5.hash(data: data)
+        return hash.map { String(format: "%02hhx", $0) }.joined()
     }
 
     var isWhitespace: Bool {
