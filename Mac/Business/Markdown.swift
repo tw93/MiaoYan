a20eaaf86529c2d95d357f28d8fa91e7ecef5583
Tw93 <tw93@qq.com>
2024-05-26 18:14:32 +0800

    :art: Optimize code

--- Mac/Business/Markdown.swift
+++ Mac/Business/Markdown.swift
@@ -6,20 +6,12 @@ func renderMarkdownHTML(markdown: String) -> String? {
     guard let parser = cmark_parser_new(CMARK_OPT_FOOTNOTES) else { return nil }
     defer { cmark_parser_free(parser) }
 
-    if let ext = cmark_find_syntax_extension("table") {
-        cmark_parser_attach_syntax_extension(parser, ext)
-    }
-
-    if let ext = cmark_find_syntax_extension("autolink") {
-        cmark_parser_attach_syntax_extension(parser, ext)
-    }
-
-    if let ext = cmark_find_syntax_extension("strikethrough") {
-        cmark_parser_attach_syntax_extension(parser, ext)
-    }
-
-    if let ext = cmark_find_syntax_extension("tasklist") {
-        cmark_parser_attach_syntax_extension(parser, ext)
+    // 附加常见的 GFM 扩展
+    let extensions = ["table", "autolink", "emoji", "footnotes", "strikethrough", "tasklist"]
+    for extName in extensions {
+        if let ext = cmark_find_syntax_extension(extName) {
+            cmark_parser_attach_syntax_extension(parser, ext)
+        }
     }
 
     cmark_parser_feed(parser, markdown, markdown.utf8.count)
@@ -29,5 +21,6 @@ func renderMarkdownHTML(markdown: String) -> String? {
     if UserDefaultsManagement.editorLineBreak == "Github" {
         res = String(cString: cmark_render_html(node, CMARK_OPT_UNSAFE | CMARK_OPT_NOBREAKS, nil))
     }
+
     return res
 }
